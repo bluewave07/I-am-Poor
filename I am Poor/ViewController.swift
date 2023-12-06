4e62e2aa404ee8425c0ec3ab3cfd4cc07916e273
Abdulkadir Akyurt <akyurt.a@gmx.de>
2023-12-06 10:26:33 +0100

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Abdulkadir AKYURT on 06.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
